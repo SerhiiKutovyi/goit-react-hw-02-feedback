3dc3858b80ea4b971d9ca4ae6b0d43a9e8afff31
SerhiiKutovyi <wagenrex@gmail.com>
2023-01-19 23:11:17 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,99 @@
+import styled from 'styled-components';
 import React, { Component } from 'react';
 
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
+    <div>
+      <StatisticsExpresso />
     </div>
   );
 };
 
-class Statistics extends Component {
+export class StatisticsExpresso extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleClick = event => {
+    console.log(event.target);
+    this.setState(prevState => {
+      return { good: prevState.good + 1 };
+    });
+  };
+
   render() {
     return (
-      <div>
-        <h1>Please leave feedback</h1>
-      </div>
+      <Statistics>
+        <h2>Please leave feedback</h2>
+        <Button>
+          <button type="button" onClick={this.handleClick}>
+            Good
+          </button>
+          <button type="button" onClick={() => console.log('Neutral')}>
+            Neutral
+          </button>
+          <button type="button" onClick={() => console.log('Bad')}>
+            Bad
+          </button>
+        </Button>
+
+        <h3>Statistics</h3>
+        <Counter className="counter">
+          <span className="counterGood">Good: {this.state.good}</span>
+          <span className="counterNeutral">Neutral: {this.state.neutral}</span>
+          <span className="counterBad">Bad: {this.state.bad}</span>
+        </Counter>
+      </Statistics>
     );
   }
 }
+
+const Statistics = styled.div`
+  h2 {
+    margin-left: 20px;
+    margin-bottom: 20px;
+    color: green;
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 48px;
+    line-height: 72px;
+  }
+
+  h3 {
+    margin-left: 20px;
+    margin-bottom: 20px;
+    color: #5418e0;
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 48px;
+    line-height: 72px;
+  }
+`;
+
+const Button = styled.div`
+  display: flex;
+
+  gap: 20px;
+  background: transparent;
+  border-radius: 3px;
+
+  margin-top: 40px;
+  margin-bottom: 40px;
+  margin-left: 20px;
+
+  button {
+    padding: 10px 30px;
+    border-radius: 4px;
+    background-color: coral;
+  }
+`;
+
+const Counter = styled.span`
+  margin-left: 20px;
+  display: flex;
+  gap: 25px;
+`;
