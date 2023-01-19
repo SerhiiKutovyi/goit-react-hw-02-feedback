8582b371e50b588735cecf349eb6294ebbf9779c
SerhiiKutovyi <wagenrex@gmail.com>
2023-01-19 16:32:25 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import React, { Component } from 'react';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +9,20 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
     </div>
   );
 };
+
+class Statistics extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Please leave feedback</h1>
+      </div>
+    );
+  }
+}
